dfb97129925bb7eba29457dd8f16b2dc637cf28d
Ana Claudia <anacnogueira@gmail.com>
2023-11-19 15:59:17 -0300

    Estilizando component App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Header } from "./components/Header";
+import styles from "./App.module.css";
 import "./global.css";
 
 export function App() {
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
+        <main>
+          NewTask
+          <br />
+          Tasks
+        </main>
+      </div>
     </>
   );
 }
